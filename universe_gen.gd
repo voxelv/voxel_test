df20a9a6592b5ced6f772668b472e1a864f22dfc
voxelv <slippytim@gmail.com>
2020-04-10 01:06:28 -0700

    cooler generator

--- universe_gen.gd
+++ universe_gen.gd
@@ -1,13 +1,16 @@
 extends VoxelStream
 class_name UniverseGen
 
+const opts = [0, 1, 4, 8, 16]
+const opts_cnt = len(opts)
+
 export var channel:int = VoxelBuffer.CHANNEL_TYPE
 
 func get_used_channels_mask () -> int:
 		return 1<<channel
  
 func emerge_block(buffer:VoxelBuffer, origin:Vector3, lod:int) -> void:
-#	var num = int(pow(2, randi() % 5))
+	var num := int(opts[randi() % opts_cnt])
 #	var num = 4
 #	if lod != 0: return
 #	if origin.y < 0:
@@ -19,8 +22,8 @@ func emerge_block(buffer:VoxelBuffer, origin:Vector3, lod:int) -> void:
 	for x in range(buffer.get_size_x()):
 		for y in range(buffer.get_size_y()):
 			for z in range(buffer.get_size_z()):
-				if x % 4 == 0 and y % 4 == 0 and z % 4 == 0:
-#				if x % num == 0 and y % num == 0 and z % num == 0:
+#				if x % 4 == 0 and y % 4 == 0 and z % 4 == 0:
+				if x % num == 0 and y % num == 0 and z % num == 0:
 #				if (x + y + z) % 2 == 0:
 					buffer.set_voxel(1, x, y, z, channel)
 
